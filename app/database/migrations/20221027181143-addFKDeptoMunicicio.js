1a0290b406596945d1f7cb21e944b223097149bf
Mario San <mario.hernandezs@salud.gob.sv>
2022-10-27 14:40:34 -0600

    feat: probando migracion de FK

---
+++ app/database/migrations/20221027181143-addFKDeptoMunicicio.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    queryInterface.addConstraint('municipios', {
+      fields: ['id_departamento'],
+      type: 'foreign key',
+      name: 'fk_municipio_departamento',
+      references: { //Required field
+        table: 'departamentos',
+        field: 'id'
+      },
+      onDelete: 'cascade',
+      onUpdate: 'cascade'
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeConstraint('municipios', 'fk_municipio_departamento');
+  }
+};
